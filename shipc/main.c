d0dcdcd7044fdc7fe451574656349841c72889f6
1Dips <ido.geva.b@gmail.com>
2024-04-24 19:33:26 +0300

    remove everything, for later refactoring

--- shipc/main.c
+++ shipc/main.c
@@ -1,25 +1,6 @@
 #include <stdio.h>
-
-
 #include "token.h"
-#include "chunk.h"
-#include "vm.h"
 
 int main() {
-	Chunk chn;
-	init_chunk(&chn);
-	add_code(&chn, OP_HALT);
-
-	print_chunk(&chn);
-
-	VM vm;
-	init_vm(&vm);
-	set_vm_chunk(&vm, &chn);
-
-	interpret(&vm);
-
-	free_vm(&vm);
-	free_chunk(&chn);
-
 	return 0;
 }
--- shipc/value.h
+++
@@ -1,8 +0,0 @@
-#pragma once
-#ifndef SHIP_VALUE_H_
-#define SHIP_VALUE_H_
-
-typedef double Value;
-
-
-#endif // !SHIP_VALUE_H_
