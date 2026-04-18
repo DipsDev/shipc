1eff334e6219aa585a8634ee8311651f79589a8e
Ido Geva <ido.geva.b@gmail.com>
2026-04-18 14:33:48 +0300

    add absolute loc to the jump instruction

--- shipc/debug.c
+++ shipc/debug.c
@@ -62,9 +62,17 @@ static int constant_instruction(Chunk* chunk, int offset) {
 }
 
 static int jump_instruction(Chunk* chunk, char* op_code, int offset) {
-	uint8_t d1 = chunk->codes[offset + 1];
-	uint8_t d2 = chunk->codes[offset + 2];
-	printf("| %04d %s (%u) |\n",  offset, op_code, (((uint16_t)d1 << 8) | d2));
+	uint16_t dist = (((uint16_t)chunk->codes[offset + 1] << 8) | chunk->codes[offset + 2]);
+
+	// Logic for the label print
+	int target;
+	if (chunk->codes[offset] == OP_JUMP_BACKWARD) {
+		target = offset + 3 - dist; // Backward
+	} else {
+		target = offset + 3 + dist; // Forward
+	}
+
+	printf("| %04d %-16s (target: %04d) |\n", offset, op_code, target);
 	return 3;
 }
 
